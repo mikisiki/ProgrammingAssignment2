afc7b5685b31d84e8577f25eea54d8d49f0dce2f
mikisiki <mikolaj.nawrocki@gmail.com>
2018-01-30 16:32:21 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function creates an environment with variable inv and x, and 
+## list of functions to manipulate set and retrieve the values of x and inv 
+## from the makeCacheMatrix environment
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      #set replaces the value of the matrix itself and erases any previous inverse matrices
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      #setinv utilizes superasignement to update inv variable held in the CacheMatrix environment
+      setinv <- function(inverse_matrix) {inv <<- inverse_matrix}
+      getinv <- function() inv
+      list(set=set, get=get,
+           setinv = setinv,
+           getinv = getinv)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      inv <- x$getinv()
+      if(!is.null(inv)){
+            message("getting cached data")
+            return(inv)
+      }
+      ## if the cachematrix does not hold the value of inverse then 
+      ## the function computes it and stores it within the cache matrix
+      data <- x$get()
+      inv <- solve(data,...)
+      x$setinv(inv)
+      inv
 }
